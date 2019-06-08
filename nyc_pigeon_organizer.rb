7ce1f12b1d4710fe907a5d2c4379d5c567668e7b
Huy Do <huyddo40@gmail.com>
2019-06-08 21:42:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	hash = {}
+	data.each do |attribute,value_hash|
+    # puts "#{attribute},#{value_hash}"
+		if attribute == :gender
+			value_hash.each do |gender,gender_hash|
+				# puts "#{gender}, #{gender_hash}"
+				gender_hash.each do |name|
+					hash[name] = {}
+          hash[name][:gender] = [gender]
+					puts "#{hash}"
+				end
+			end
+		end
+
+  end
+  binding.pry
+	hash
+
 end
