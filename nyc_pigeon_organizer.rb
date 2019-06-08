862e984f6bc0618088de4757a10b0e5e79c89805
Huy Do <huyddo40@gmail.com>
2019-06-08 21:42:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,20 +2,31 @@ require "pry"
 def nyc_pigeon_organizer(data)
 	hash = {}
 	data.each do |attribute,value_hash|
-    # puts "#{attribute},#{value_hash}"
 		if attribute == :gender
 			value_hash.each do |gender,gender_hash|
-				# puts "#{gender}, #{gender_hash}"
 				gender_hash.each do |name|
-					hash[name] = {}
-          hash[name][:gender] = [gender]
-					puts "#{hash}"
+					hash[name] ||= {} #set empty array if hash doesn't have a key, otherwise just take existing key:value
+					# hash[name] = {}
+          hash[name][:gender] = [gender.to_s]
+					# puts "#{hash}"
 				end
 			end
 		end
-
   end
-  binding.pry
-	hash
 
+  data.each do |attribute,value_hash|
+    if attribute == :color
+      value_hash.each do |color,name_arr|
+				puts "#{color},#{name_arr}"
+        name_arr.each do |name|
+         	if hash[name] 
+						hash[name][:color] ||= []
+          	hash[name][:color] << color.to_s
+         	end
+        end
+      end
+    end
+  end
+  # binding.pry
+	hash
 end
